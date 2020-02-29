a8a536ea453a1049dc8837c62634b0e1fa400ef3
styrmank <kasper.styrman@tuni.fi>
2020-02-29 23:05:34 +0200

    First version of app ready for launch

---
+++ index.js
@@ -0,0 +1,47 @@
+﻿const express = require('express')
+const jsondb = require('simple-json-db')
+const app = express()
+const db = new jsondb('clicks.json')
+
+// map containing the amount of clicks as the key and warded points as value
+const awardPointsMap = new Map([[10, 5], [100, 40], [500, 250]])
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+
+})
+
+
+app.get('/clickhandler', function (req, res) {
+    db.JSON({ 'clicks': db.get('clicks') + 1 })
+    db.sync()
+    res.send(checkWin(db.get('clicks')))
+})
+
+/**
+ * ● 5 pistettä joka 10. painallus
+ * ● 40 pistettä joka 100. painallus
+ * ● 250 pistettä joka 500. painallus.
+ * */
+
+
+function checkWin(totalClicks) {
+    var hasWon = false
+    var awardPoints = 0
+
+    awardPointsMap.forEach((awardedPoints, clicksNeeded) => {
+        if (totalClicks % clicksNeeded == 0) {
+            hasWon = true
+            awardPoints = awardedPoints
+        }
+    })
+    
+    return {
+        win: hasWon,
+        award: awardPoints,
+        remainingClicks: 10 - (totalClicks % 10)
+    }
+}
+
+app.listen(3000)
